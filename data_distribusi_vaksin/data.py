b232c6fb6b1dcd7798037df9bc6b339a29acd420
rifqifazaardiansyah-not <rifqifzaa.14@gmail.com>
2026-05-22 16:03:54 +0700

    perbaikan data

--- data_distribusi_vaksin/data.py
+++ data_distribusi_vaksin/data.py
@@ -1,18 +1,19 @@
-# =========================
-# DATA
-# =========================
-
+# ==========================================
+# 1. DEFINISI DATA SESUAI STUDI KASUS JURNAL
+# ==========================================
 origin = ['CGK', 'UPG']
-
 destination = ['BPN', 'SUB', 'KNO', 'UPG', 'DJJ']
 
 M = 999999999
 
+# Matriks Biaya Pengiriman (Rp/kg)
 matrix = [
     [24000, 22500, 30000, 28000, 45000],  # CGK
     [M, M, M, M, 18000]                   # UPG
 ]
 
-need = [2000, 3000, 2000, 2500, 2500]
+# Kebutuhan Vaksin (Demand) tiap kota tujuan
+need = [2000, 3000, 2000, 2500, 2500]      # Total = 12,000 kg
 
-availability = [11500, 2500]
+# Kapasitas Gudang (Supply) real berdasarkan alokasi imbang jurnal
+availability = [11000, 1000]               # Total = 12,000 kg
